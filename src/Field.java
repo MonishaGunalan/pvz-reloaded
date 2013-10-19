057e5f4dfa4b989b550996823980e03d6667b3c7
monisha <moni.sind@gmail.com>
2013-10-19 10:57:30 -0400

    Basic skeleton of class level, field and square. The methods are yet to be implemented.

---
+++ src/Field.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+
+
+public class Field {
+
+	private static final int DEFAULT_MAX_ROW = 5;
+	private static final int DEFAULT_MAX_POSN = 5;
+	private Square[][] square;
+	private Set<TurnBasedElements> elements;
+	
+	public Field(int row, int col){
+		
+	}
+	
+	public void addZombie(int row, int col){
+		
+	}
+	
+	public void addPlant(int row, int col){
+		
+	}
+}
---
+++ src/Square.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+
+public class Square {
+
+	private Plant plant;
+	private List<Zombie> zombies;
+	private List<Bullet> bullets;
+	private int row;
+	private int col;
+	
+	
+	private enum terrain{
+		
+	}
+	
+	public Square(){
+		
+	}
+	
+	private int getRow(){
+		return row;
+	}
+	
+	private int getCol(){
+		return col;
+	}
+}
