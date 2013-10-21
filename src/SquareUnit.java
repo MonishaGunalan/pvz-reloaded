a9fdfc68b36e2172ef387a696703c159c17b7092
Arzaan Irani <arzaan92@hotmail.com>
2013-10-21 04:22:30 -0400

    Added in SquareUnit

---
+++ src/SquareUnit.java
@@ -0,0 +1,24 @@
+
+public class SquareUnit {
+	
+	protected Square square;
+	private int row;
+	private int col;
+	
+	public Square getSquare(){
+		return square;
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	
+	public int getCol(){
+		return col;
+	}
+	
+	public void setSquare(Square square){
+		this.square = square;
+	}
+
+}
