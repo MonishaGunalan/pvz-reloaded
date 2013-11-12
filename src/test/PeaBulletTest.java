93ca2b07fad44f18b87c8a78dca26e79f00f4f7b
arzaanirani <arzaan92@hotmail.com>
2013-11-11 19:51:10 -0500

    Update PeaBulletTest.java
    Finished PeaBulletTest.java

--- src/test/PeaBulletTest.java
+++ src/test/PeaBulletTest.java
@@ -5,14 +5,6 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-
-import static org.junit.Assert.*;
-import junit.framework.TestCase;
-
-import org.junit.Before;
-import org.junit.Test;
-
-
 public class PeaBulletTest extends TestCase{
 
 	private Level testLevel1;
@@ -20,29 +12,23 @@ public class PeaBulletTest extends TestCase{
 	private Strip testStrip1;
 	private Square testSquare1;
 	private Zombie testZombie1;
+	private PeaBullet testPeaBullet;
 	private String[] terrainType = { "mud", "grass", "grass", "grass", "mud" };
 
 	@Before
-		public void setUp() throws Exception {
-			testLevel1 = new Level(1);
-			testField1 = new Field(terrainType, testLevel1);
-			testStrip1 = new Strip("grass", 3, testField1);
-			testSquare1 = new Square("grass", 3, 2, testStrip1);
-			testZombie1 = new NormalZombie();
+	public void setUp() throws Exception {
+		testLevel1 = new Level(1);
+		testField1 = new Field(terrainType, testLevel1);
+		testStrip1 = new Strip("grass", 3, testField1);
+		testSquare1 = new Square("grass", 3, 2, testStrip1);
+		testZombie1 = new NormalZombie();
 
-			testZombie1.setSquare(testSquare1);
-		}
-		
-	
-		public Bullet.Type getType() {
-			assertTrue(PeaBullet.getType(Bullet.Type.PEA) == PeaBullet.class);
-		}
-		
-		
-		
+		testZombie1.setSquare(testSquare1);
 	}
 
-
-
+	public void getType() {
+		assertTrue(testPeaBullet.getType().name()=="PEA");
+	}
 }
+
 
