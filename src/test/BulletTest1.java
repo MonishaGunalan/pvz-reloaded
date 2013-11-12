e93d8c4cc7f27a06bb90cec23f2777f26473ffc0
monisha <moni.sind@gmail.com>
2013-11-11 19:35:10 -0500

    Created test class for bullet

---
+++ src/test/BulletTest1.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BulletTest1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testHit() {
+
+	}
+
+	public void testGetDmg() {
+
+	}
+
+	public void testGetMoveTrigger() {
+
+	}
+
+	public void testMove() {
+
+	}
+	
+	public void testUpdate(){
+		
+	}
+
+}
