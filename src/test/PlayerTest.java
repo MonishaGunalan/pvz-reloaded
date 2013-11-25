7370e986314a5dcd9201f1a0a0db111f10f167a7
Christopher Nguyen <chrisnguyenca@gmail.com>
2013-11-24 19:34:37 -0500

    fixed playertest

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -28,7 +28,7 @@ public class PlayerTest {
 		field = level.getField();
 		strip = field.getStrip()[row];
 
-		player = new Player(model);
+		player = new Player(level,model);
 		square = strip.getSquare(col);
 		square2 = strip.getSquare(col+1);
 
