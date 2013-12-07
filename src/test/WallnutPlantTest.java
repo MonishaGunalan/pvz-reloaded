88b088a780da6fb28efd47c01907e18ced25b2d0
arzaanirani <arzaan92@hotmail.com>
2013-12-06 19:27:36 -0500

    Create WallnutPlantTest.java

---
+++ src/test/WallnutPlantTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import pvz.level.Field;
+import pvz.level.Level;
+import pvz.level.Square;
+import pvz.level.Strip;
+import pvz.unit.Plant;
+import pvz.unit.SunflowerPlant;
+import pvz.unit.WallnutPlant;
+import pvz.unit.Zombie;
+/**
+ * This class contains unit testing for all 
+ * methods in the abstract class WallnutPlant
+ * @author Arzaan irani
+ * 100826631
+ 
+ */
+public class WallnutPlantTest {
+	private Level testLevel1;
+	private Field testField1;
+	private Strip testStrip1;
+	private Square testSquare1;
+	private Zombie testZombie1;
+	private String[] terrainType = { "mud", "grass", "grass", "grass", "mud" };
+
+	@Before
+	public void setUp() throws Exception {
+		testLevel1 = new Level(1);
+		testField1 = new Field(terrainType, testLevel1);
+		testStrip1 = new Strip("grass", 3, testField1);
+		testSquare1 = new Square( 3, 2, testStrip1);
+	}
+	
+	@Test
+	public void constructor(){
+		Plant p = new WallnutPlant(testSquare1);
+		assertTrue("Constructor should set square to specified one", p.getSquare() == testSquare1);
+	}
+}
